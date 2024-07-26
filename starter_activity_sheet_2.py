c7b562f4add734f173bb2508a6495c1f50d96b17
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-26 14:46:48 -0400

    Add files via upload

---
+++ starter_activity_sheet_2.py
@@ -0,0 +1,15 @@
+# starter program week 2 - perhaps this could be a task
+
+def conversation():
+    print("Welcome to my conversation program")
+    print()
+    print("Do you like cycling? Answer yes or no")
+    answer = input()
+    if answer == "yes":
+        print("That's good - you will get very fit")
+    else:
+        print("Perhaps you like some other sport. ")
+    print("Goodbye")
+
+conversation()
+
